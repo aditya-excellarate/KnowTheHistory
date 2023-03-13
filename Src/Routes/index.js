a62e85ed1b2d1f13c2da9296e2c1b829f5902759
aditya sharma <aditya.sharma@excellarate.com>
2023-03-13 08:05:31 +0530

    -project setup

---
+++ Src/Routes/index.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../Screen/Home';
+
+const Stack = createNativeStackNavigator();
+const Route = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Route;
+
+const styles = StyleSheet.create({});
